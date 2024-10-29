2d16174b0f6d49f3fe8720954b22a0178fba09b3
Dev-Salem <108498811+Dev-Salem@users.noreply.github.com>
2024-10-29 17:33:44 +0300

    Refactor route generation and add no animation transition
    - Refactor the route generation in the main.dart file to use a switch statement for better readability and maintainability.
    - Add a new class `NoAnimationTransition` in the animations.dart file to handle route transitions without any animation.
    - Update the onGenerateRoute method in the main.dart file to use the `NoAnimationTransition` class for specific routes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:task_tracker/tasks/core/app_colors.dart';
+import 'package:task_tracker/tasks/core/animations.dart';
+import 'package:task_tracker/tasks/domain/task.dart';
+import 'package:task_tracker/tasks/presentation/screens/task_details_screen.dart';
 import 'package:task_tracker/tasks/presentation/screens/tasks_screen.dart';
 
 void main() {
@@ -18,11 +19,25 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
           scaffoldBackgroundColor: const Color.fromARGB(156, 27, 27, 27),
           fontFamily: "Helvetica",
+          textButtonTheme: const TextButtonThemeData(style: ButtonStyle()),
+          disabledColor: Colors.red,
           brightness: Brightness.dark),
       // theme: ThemeData(
       //   useMaterial3: true,
       // ),
       themeMode: ThemeMode.dark,
+      onGenerateRoute: (settings) {
+        return switch (settings.name) {
+          'tasks' => NoAnimationTransition(
+              builder: (context) => const TasksScreen(),
+            ),
+          'task_details' => NoAnimationTransition(
+              builder: (context) =>
+                  TaskDetailsScreen(task: settings.arguments as Task),
+            ),
+          _ => NoAnimationTransition(builder: (context) => const TasksScreen())
+        };
+      },
       home: const TasksScreen(),
     );
   }
