d8574df278928065c18377e7ab4906a15e2e89ff
Dev-Salem <108498811+Dev-Salem@users.noreply.github.com>
2024-11-02 10:35:57 +0300

    add animation to background color

--- lib/tasks/presentation/screens/task_details_screen.dart
+++ lib/tasks/presentation/screens/task_details_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:gap/gap.dart';
+import 'package:task_tracker/tasks/core/app_colors.dart';
 import 'package:task_tracker/tasks/domain/task.dart';
 import 'package:task_tracker/tasks/presentation/widgets/custom_toggle_button.dart';
 import 'package:task_tracker/tasks/presentation/widgets/screen_details_appbar.dart';
@@ -21,8 +22,9 @@ class TaskDetailsScreen extends StatefulWidget {
 class _TaskDetailsScreenState extends State<TaskDetailsScreen>
     with SingleTickerProviderStateMixin {
   bool selectedButton = true;
+  Color _backGroundColor = AppColors.backgroundColor;
   late final AnimationController _controller;
-  
+
   double calculateDynamicHeight(
       int index, double availableHeight, int itemCount) {
     if (index == 0) return availableHeight;
@@ -39,6 +41,11 @@ class _TaskDetailsScreenState extends State<TaskDetailsScreen>
   @override
   void initState() {
     _controller = AnimationController(vsync: this);
+    Future.delayed(0.ms, () {
+      setState(() {
+        _backGroundColor = widget.task.cardColor;
+      });
+    });
     super.initState();
   }
 
@@ -51,66 +58,67 @@ class _TaskDetailsScreenState extends State<TaskDetailsScreen>
   @override
   Widget build(BuildContext context) {
     final subtasksCount = widget.task.subtasks.length;
-    return Scaffold(
-      backgroundColor: widget.task.cardColor,
-      body: DefaultTextStyle.merge(
-          style: const TextStyle(color: Colors.black),
-          child: Column(
-            children: [
-              const Gap(48),
-              ScreenDetailsAppBar(subtasks: widget.task.subtasks),
-              const Gap(32),
-              Expanded(
-                child: LayoutBuilder(builder: (context, cons) {
-                  return Stack(
-                    children: [
-                      Align(
-                        alignment: Alignment.topCenter,
-                        child: Column(
-                          children: [
-                            Text(
-                              "Tasks".toUpperCase(),
-                              style: const TextStyle(fontSize: 96, height: 1),
-                              textAlign: TextAlign.center,
-                            ),
-                            CustomToggleButton(
-                                selectedButton: selectedButton,
-                                onPressed: (index) {
-                                  setState(() {
-                                    selectedButton = !selectedButton;
-                                  });
-                                }),
-                          ],
+    return AnimatedContainer(
+      duration: 400.ms,
+      color: _backGroundColor,
+      curve: Curves.decelerate,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: DefaultTextStyle.merge(
+            style: const TextStyle(color: Colors.black),
+            child: Column(
+              children: [
+                const Gap(48),
+                ScreenDetailsAppBar(subtasks: widget.task.subtasks),
+                const Gap(32),
+                Expanded(
+                  child: LayoutBuilder(builder: (context, cons) {
+                    return Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Tasks".toUpperCase(),
+                                style: const TextStyle(fontSize: 96, height: 1),
+                                textAlign: TextAlign.center,
+                              ),
+                              CustomToggleButton(
+                                  selectedButton: selectedButton,
+                                  onPressed: (index) {
+                                    setState(() {
+                                      selectedButton = !selectedButton;
+                                    });
+                                  }),
+                            ],
+                          ),
                         ),
-                      ),
-                      ...List.generate(subtasksCount, (index) {
-                        return Positioned(
-                            top: 200 +
-                                calculateDynamicTopOffset(
-                                    index, cons.maxHeight, subtasksCount),
-                            height: calculateDynamicHeight(
-                                index, cons.maxHeight, subtasksCount),
-                            left: 0,
-                            right: 0,
-                            child: SubtaskCardWidget(
-                                index: index,
-                                subtask: widget.task.subtasks[index]));
-                      }),
-                    ]
-                        .animate(
-                          interval: 200.ms,
-                        )
-                        .slideY(
-                          begin: 1,
-                          end: 0,
-                          duration: 900.ms,
-                          curve: Curves.fastOutSlowIn,
-                        ),
-                  );
-                }),
-              )
-            ],
-          )),
+                        ...List.generate(subtasksCount, (index) {
+                          return Positioned(
+                              top: 200 +
+                                  calculateDynamicTopOffset(
+                                      index, cons.maxHeight, subtasksCount),
+                              height: calculateDynamicHeight(
+                                  index, cons.maxHeight, subtasksCount),
+                              left: 0,
+                              right: 0,
+                              child: SubtaskCardWidget(
+                                  index: index,
+                                  subtask: widget.task.subtasks[index]));
+                        }),
+                      ].animate(interval: 200.ms, delay: 1000.ms).slideY(
+                            begin: 1,
+                            end: 0,
+                            duration: 900.ms,
+                            curve: Curves.fastOutSlowIn,
+                          ),
+                    );
+                  }),
+                )
+              ],
+            )),
+      ),
     );
   }
 }
